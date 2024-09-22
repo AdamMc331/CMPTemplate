d8233e94970878fca98a7fecd039ad9df0f6652d
Adam McNeilly <amcneilly331@gmail.com>
2024-09-22 12:39:34 -0400

    Adding viewmodel & runtime components.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -36,8 +36,8 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-//            implementation(libs.androidx.lifecycle.viewmodel)
-//            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
         }
     }
 }
