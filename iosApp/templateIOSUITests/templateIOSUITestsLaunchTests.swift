01c803329e30bb55564c4240615e9d5d484088ac
Adam McNeilly <amcneilly331@gmail.com>
2024-09-24 16:27:41 -0400

    Adding iOS app project. (#7)
    Fixes #8.

---
+++ iosApp/templateIOSUITests/templateIOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  templateIOSUITestsLaunchTests.swift
+//  templateIOSUITests
+//
+//  Created by Adam McNeilly on 9/24/24.
+//
+
+import XCTest
+
+final class templateIOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
