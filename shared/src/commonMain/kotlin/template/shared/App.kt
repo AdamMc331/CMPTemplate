627bf0e2926ee9eba892f8e83196de05578aff7b
Adam McNeilly <amcneilly331@gmail.com>
2024-09-30 22:30:34 -0400

    Cleaning up the App composable.

--- shared/src/commonMain/kotlin/template/shared/App.kt
+++ shared/src/commonMain/kotlin/template/shared/App.kt
@@ -17,8 +17,8 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@Composable
 @Preview
+@Composable
 fun App() {
     MaterialTheme {
         var showContent by remember {
