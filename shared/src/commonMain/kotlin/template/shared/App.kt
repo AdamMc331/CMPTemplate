a3d31487419eb92ed4343f29029c200884f6dc80
Adam McNeilly <amcneilly331@gmail.com>
2024-09-30 22:47:55 -0400

    Updating README, removing actions documentation. (#32)

--- shared/src/commonMain/kotlin/template/shared/App.kt
+++ shared/src/commonMain/kotlin/template/shared/App.kt
@@ -17,20 +17,45 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@Composable
 @Preview
+@Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        var showContent by remember {
+            mutableStateOf(false)
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Button(
+                onClick = {
+                    showContent = !showContent
+                },
+            ) {
+                Text(
+                    text = "Click me!",
+                )
             }
+
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                val greeting = remember {
+                    Greeting().greet()
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                ) {
+                    Image(
+                        painter = painterResource(Res.drawable.compose_multiplatform),
+                        contentDescription = null,
+                    )
+                    Text(
+                        text = "Compose: $greeting",
+                    )
                 }
             }
         }
