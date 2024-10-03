e256a57f2ace05881a158fdc93aa8e468b472470
Adam McNeilly <amcneilly331@gmail.com>
2024-10-02 20:31:53 -0400

    Using Surface to support light/dark theme.

--- shared/src/commonMain/kotlin/template/shared/App.kt
+++ shared/src/commonMain/kotlin/template/shared/App.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,37 +26,39 @@ fun App() {
             mutableStateOf(false)
         }
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            Button(
-                onClick = {
-                    showContent = !showContent
-                },
+        Surface {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth(),
             ) {
-                Text(
-                    text = "Click me!",
-                )
-            }
-
-            AnimatedVisibility(showContent) {
-                val greeting = remember {
-                    Greeting().greet()
+                Button(
+                    onClick = {
+                        showContent = !showContent
+                    },
+                ) {
+                    Text(
+                        text = "Click me!",
+                    )
                 }
 
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                ) {
-                    Image(
-                        painter = painterResource(Res.drawable.compose_multiplatform),
-                        contentDescription = null,
-                    )
-                    Text(
-                        text = "Compose: $greeting",
-                    )
+                AnimatedVisibility(showContent) {
+                    val greeting = remember {
+                        Greeting().greet()
+                    }
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                    ) {
+                        Image(
+                            painter = painterResource(Res.drawable.compose_multiplatform),
+                            contentDescription = null,
+                        )
+                        Text(
+                            text = "Compose: $greeting",
+                        )
+                    }
                 }
             }
         }
