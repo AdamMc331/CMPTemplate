31eecda9772364b29c20977caa46408ea267691b
Adam McNeilly <amcneilly331@gmail.com>
2024-10-06 00:50:35 -0400

    Adding BaseKtorClient helper (#51)

--- shared/src/commonMain/kotlin/template/shared/DatabaseDriverFactory.kt
+++ shared/src/commonMain/kotlin/template/shared/DatabaseDriverFactory.kt
@@ -3,8 +3,10 @@ package template.shared
 import app.cash.sqldelight.db.SqlDriver
 
 /**
- * Interface to provide a SqlDelight [SqlDriver] instance. We will
- * need to define this per platform.
+ * Interface to provide a SqlDelight [SqlDriver] instance. We will need to define this per platform.
+ *
+ * Note that the template does not provide a way to enforce/supply the implementation in common code. Whether
+ * you convert this to an expect/actual interface, or use DI to provide this dependency is up to you.
  */
 interface DatabaseDriverFactory {
     fun createDriver(): SqlDriver
