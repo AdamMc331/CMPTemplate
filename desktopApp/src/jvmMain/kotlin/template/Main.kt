0c9149190aadc8ada149d6fead84e32ab83f6c49
Adam McNeilly <amcneilly331@gmail.com>
2024-11-27 12:08:43 -0500

    Adding Desktop target.

---
+++ desktopApp/src/jvmMain/kotlin/template/Main.kt
@@ -0,0 +1,15 @@
+package template
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import template.shared.App
+
+fun main() =
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "KotlinProject",
+        ) {
+            App()
+        }
+    }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -33,6 +33,8 @@ kotlin {
         }
     }
 
+    jvm("desktop")
+
     sourceSets {
         commonMain.dependencies {
             implementation(compose.runtime)
