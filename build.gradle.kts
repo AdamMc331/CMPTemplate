3b4b40a6a0c289a3d7aff2ef50e4cc8def18a7c4
Adam McNeilly <amcneilly331@gmail.com>
2026-01-25 16:56:25 -0500

    Removing versions plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.kmp.library).apply(false)
     alias(libs.plugins.apollo.graphql).apply(false)
-    alias(libs.plugins.benmanes.versions).apply(false)
     alias(libs.plugins.cash.sqldelight).apply(false)
     alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.detekt).apply(true) // Needs to be applied at the root, unlike others.
@@ -15,7 +14,6 @@ plugins {
 
 apply(from = "buildscripts/githooks.gradle")
 apply(from = "buildscripts/setup.gradle")
-apply(from = "buildscripts/versionsplugin.gradle")
 
 subprojects {
     apply(from = "../buildscripts/detekt.gradle")
